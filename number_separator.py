2bab4b8219017218e44d9f01d6e5596b85a01a9f
nashalexisdolores <alxsdlrs@gmail.com>
2026-04-27 18:40:42 +0800

    add input file validation logic

--- number_separator.py
+++ number_separator.py
@@ -1,3 +1,10 @@
+import os
+
 class NumberProcessor:
     def __init__(self, input_file="numbers.txt"):
         self.input_file = input_file
+
+    def split_even_odd(self):
+        if not os.path.exists(self.input_file):
+            print("Error: Input file not found.")
+            return
